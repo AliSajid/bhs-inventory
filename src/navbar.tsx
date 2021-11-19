c8cde09cc769c4292990719ee1c7b7ecd9db1a27
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-11-19 10:28:04 -0500

    Added the navbar component

---
+++ src/navbar.tsx
@@ -0,0 +1,15 @@
+export const NavBar = (props: any) => {
+return <div>
+    <ul>
+        <li>
+            <a href="http://google.com">Google</a>
+        </li>
+        <li>
+            <a href="http://google.com">Google</a>
+        </li>
+        <li>
+            <a href="http://google.com">Google</a>
+        </li>
+    </ul>
+</div>;
+}
