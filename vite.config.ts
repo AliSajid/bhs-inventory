7d4b674d866cc87ab343f2925dfca5600a4e7d5c
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-11-20 06:25:25 -0500

    Ensured the typescript compiles and react is aliased correctly

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import alias from '@rollup/plugin-alias';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  resolve: {
+    alias: [
+      { find: 'react', replacement: 'preact/compat' },
+      { find: 'react-dom/test-utils', replacement: 'preact/test-utils' },
+      { find: 'react-dom', replacement: 'preact/compat' },
+      { find: 'react/jsx-runtime', replacement: 'preact/jsx-runtime' },
+    ],
+  },
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment',
+  },
 });
