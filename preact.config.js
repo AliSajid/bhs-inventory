aee02455c80b75d428940cdc0067e8f6ea419e1b
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-11-27 21:45:49 -0500

    Added aliases to preact.config

--- preact.config.js
+++ preact.config.js
@@ -1,7 +1,14 @@
 const tailwind = require("preact-cli-tailwind");
 
 module.exports = (config, env, helpers) => {
-    config = tailwind(config, env, helpers);
-    config.resolve.alias.src = env.src;
+  config = tailwind(config, env, helpers);
+//  config.resolve.alias.src = env.src;
+  config.resolv.alias = {
+    "src": env.src,
+    "react": "preact/compat",
+    "react-dom/test-utils": "preact/test-utils",
+    "react-dom": "preact/compat", // Must be below test-utils
+    "react/jsx-runtime": "preact/jsx-runtime",
+  };
   return config;
 };
