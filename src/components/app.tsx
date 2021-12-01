bae2d91c26d679ba1ee5417cbf10aa4cc89bfc74
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-12-01 16:23:35 -0500

    Added firebase to the app

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -6,6 +6,7 @@ import Profile from "../routes/profile";
 import NotFoundPage from "../routes/notfound";
 import Header from "./header";
 
+
 const App: FunctionalComponent = () => {
   return (
     <div id="preact_root">
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import "./style/index.css";
 import App from "./components/app";
+import {app, analytics} from "./utils/firebase-config";
 
 export default App;
