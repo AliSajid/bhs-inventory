21a5c4181de5288cde8790ff93a70656ce1f106b
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-12-02 21:34:05 -0500

    Updated all the components

--- src/routes/notfound/index.tsx
+++ src/routes/notfound/index.tsx
@@ -3,13 +3,16 @@ import { Link } from "preact-router/match";
 
 const Notfound: FunctionalComponent = () => {
   return (
-    <div className="">
-      <h1>Error 404</h1>
-      <p>That page doesn&apos;t exist.</p>
-      <Link href="/">
-        <h4>Back to Home</h4>
-      </Link>
-    </div>
+<div class="max-w-sm rounded overflow-hidden shadow-lg origin-center">
+  
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2 text-center">The Coldest Sunset</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+        </p>
+        <Link href="/" activeClassName="text-center button">Back to Home</Link>
+  </div>
+</div>
   );
 };
 
