cee8083f907c515f6d1904f651f7b073bce3176b
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-12-03 21:49:45 -0500

    Added the icons and updated the not-found page

--- src/routes/notfound/index.tsx
+++ src/routes/notfound/index.tsx
@@ -1,14 +1,33 @@
 import { FunctionalComponent, h } from "preact";
 import { Link } from "preact-router/match";
+import { AlertOctagon } from "preact-feather";
 
 const Notfound: FunctionalComponent = () => {
   return (
-    <div className="">
-      <h1>Error 404</h1>
-      <p>That page doesn&apos;t exist.</p>
-      <Link href="/">
-        <h4>Back to Home</h4>
-      </Link>
+    <div className="flex-mb-4 overflow-hidden">
+      <div className="w-full h-12 p-12">
+        <h1 className="mx-auto text-red-600 font-bold text-center text-4xl">
+          Page Not Found
+        </h1>
+      </div>
+
+      <div className="w-full h-12 p-8">
+        <AlertOctagon size={56} className="mx-auto" />
+      </div>
+      <div className="w-full h-12 p-12">
+        <p className="text-center text-xl">
+          You've landed on a page that doesn't exist. <br />
+          We apologize for the inconvenience.
+        </p>
+      </div>
+
+      <div className="mx-auto w-full h-12 p-8 ml-4 mr-4 object-center">
+        <button className="rounded shadow inline-block border-2 bg-blue-400">
+          <Link href="/" class="p-4 text-white mx-10">
+            Go to Home
+          </Link>
+        </button>
+      </div>
     </div>
   );
 };
