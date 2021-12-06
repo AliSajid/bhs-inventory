dc19175b63f368fe79174069b2b37d1b7cfe56eb
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-12-06 14:57:02 -0500

    Changed default text

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -4,7 +4,9 @@ const Home: FunctionalComponent = () => {
   return (
     <div className="">
       <h1 className="text-center text-2xl text-blue-500">Home</h1>
-      <p className="text-center text-xl text-red-500">This is the Home component.</p>
+      <p className="text-center text-xl text-red-500">
+        This is the Home component.
+      </p>
     </div>
   );
 };
