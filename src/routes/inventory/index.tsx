9c1146dbbec485d3885ad9d487f99a31e9ded0db
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-12-06 14:57:31 -0500

    Activating firestore API

--- src/routes/inventory/index.tsx
+++ src/routes/inventory/index.tsx
@@ -1,11 +1,20 @@
-import { h, FunctionalComponent } from 'preact';
+import { h, FunctionalComponent } from "preact";
+import { db } from "src/utils/firebase-config";
+import {
+  doc,
+  collection,
+  getDocs,
+  addDoc,
+  serverTimestamp,
+  where,
+} from "firebase/firestore";
+
+const getInventory: Function = () => {
+  console.log("Hello, world");
+};
 
 const Inventory: FunctionalComponent = () => {
-    return (
-        <div>
-            This is Inventory Page.
-        </div>
-    )
-}
+  return <div>This is Inventory Page.</div>;
+};
 
 export default Inventory;
