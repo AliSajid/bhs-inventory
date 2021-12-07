bd2574ed2881fbfad97f3ad5849a15750398a50e
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-12-07 14:41:03 -0500

    Fixed the env vars issue

--- preact.config.js
+++ preact.config.js
@@ -1,11 +1,11 @@
-import Dotenv from 'dotenv-webpack';
-
 const tailwind = require("preact-cli-tailwind");
+
+import envVars from 'preact-cli-plugin-env-vars';
 
 
 module.exports = (config, env, helpers) => {
   config = tailwind(config, env, helpers);
-  config.plugins.push(new Dotenv({path: "./.env"}));
+  envVars(config, env, helpers);
   config.resolve.alias.src = "src";
   
   return config;
