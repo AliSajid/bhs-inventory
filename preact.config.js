8149cc3ef4688d46063b6635f28e0a75ac509821
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-12-26 21:04:57 -0500

    Added aliases

--- preact.config.js
+++ preact.config.js
@@ -1,4 +1,6 @@
 const tailwind = require("preact-cli-tailwind");
+const path = require('path');
+
 
 import envVars from 'preact-cli-plugin-env-vars';
 
@@ -6,7 +8,8 @@ import envVars from 'preact-cli-plugin-env-vars';
 module.exports = (config, env, helpers) => {
   config = tailwind(config, env, helpers);
   envVars(config, env, helpers);
-  config.resolve.alias.src = "src";
+  config.resolve.alias.src = path.resolve(__dirname, 'src');
+  config.resolve.alias.routes = path.resolve(__dirname, 'src', 'routes')
   
   return config;
 };
